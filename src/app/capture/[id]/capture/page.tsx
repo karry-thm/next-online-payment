f196359ec8dd14a487756768cc4e2322027810b6
Martin Karry <martin@karry.de>
2024-06-21 08:22:33 +0200

    Updated Output of capture and error

--- src/app/capture/[id]/capture/page.tsx
+++ src/app/capture/[id]/capture/page.tsx
@@ -1,7 +1,28 @@
 import cap from "@/components/cart/payment/capture.action";
 
-export default async function capture({ params, searchParams }: { params: { id: string }, searchParams?: { token: string } }) {
+export default async function capture({ params, searchParams }: { params: { id: string }, searchParams?: { token?: string, PayerID?: string } }) {
     console.log("CAPTURE", searchParams?.token);
     console.log(await cap(searchParams?.token || "asdf"));
-    return <div>Capture ({params.id}-{searchParams?.token})</div>
+    return <div>
+        <h1>Capture</h1>
+        <table>
+            <tbody>
+                <tr>
+                    <th colSpan={2}>Parameter</th>
+                </tr>
+                <tr>
+                    <th>Internal ID</th>
+                    <td>{params.id}</td>
+                </tr>
+                <tr>
+                    <th>PayPal ID</th>
+                    <td>{searchParams?.token}</td>
+                </tr>
+                <tr>
+                    <th>Payer ID</th>
+                    <td>{searchParams?.PayerID}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 }
--- src/app/capture/[id]/error/page.tsx
+++ src/app/capture/[id]/error/page.tsx
@@ -1,3 +1,20 @@
-export default async function capture({ params }: { params: { id: string } }) {
-    return <div>Error ({params.id})</div>
+export default async function capture({ params, searchParams }: { params: { id: string }, searchParams?: { token?: string } }) {
+    return <div>
+        <h1>Payment ERROR</h1>
+        <table>
+            <tbody>
+                <tr>
+                    <th colSpan={2}>Parameter</th>
+                </tr>
+                <tr>
+                    <th>Internal ID</th>
+                    <td>{params.id}</td>
+                </tr>
+                <tr>
+                    <th>PayPal ID</th>
+                    <td>{searchParams?.token}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 }
